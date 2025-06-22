9c6cf9141decebff9f69e90a4b43c21de4f7fcf0
Naresh Kumar M <124366029+nareshlab@users.noreply.github.com>
2025-06-22 13:26:27 +0530

    Delete Week 1/Liskov.java

--- Week 1/Liskov.java
+++
@@ -1,3 +0,0 @@
-A child class must act like its parent class. It shouldn’t break any rules or expectations set by the parent. 
-If a child class can’t properly replace the parent, the code becomes unpredictable, buggy, and hard to maintain.
-
